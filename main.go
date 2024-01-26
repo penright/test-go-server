4e7f73447f2a5b9f739ea94731307ab4fc405dfa
Paul <paul@enrights.net>
2024-01-26 14:45:05 -0600

    New main

--- main.go
+++ main.go
@@ -4,6 +4,10 @@ import "github.com/gofiber/fiber/v2"
 
 func main() {
 	app := fiber.New()
+	app.Static("/","./html")
+	app.Get("/*",func(c *fiber.Ctx) error{
+		return c.SendFile("./html/index.html")
+	})
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("hello, World")
